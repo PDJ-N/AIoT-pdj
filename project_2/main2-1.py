10b7d1def4d3b712e66cf7b875b98cd658f5e627
PDJ-N <qkreo321@gmail.com>
2026-03-31 23:30:02 +0900

    학습 후 주석 추가

--- project_2/main2-1.py
+++ project_2/main2-1.py
@@ -1,18 +1,30 @@
 from gpiozero import LEDBoard
 from time import sleep
 
+# 이번 실습에서는 LEDBoard로 여러 개의 LED를 한 번에 제어하는 방법을 연습했다.
+# 앞의 3개는 차량 신호등, 뒤의 2개는 보행자 신호등이라고 생각하고 연결했다.
+# 핀 순서를 직접 맞춰보면서 어떤 LED가 어떤 역할인지 같이 익혔다.
 leds = LEDBoard(2,3,4,20,21)
 
 try:
     while 1:
+        # 첫 번째는 차량이 지나가고 보행자는 기다리는 상태다.
+        # tuple 값으로 LED 여러 개를 한 번에 바꿀 수 있는 점이 편하다고 느꼈다.
         leds.value = (0,0,1,1,0)
         sleep(3.0)
+
+        # 두 번째는 차량 노란불 상태로, 곧 멈춘다는 흐름을 표현했다.
         leds.value = (0,1,0,1,0)
         sleep(1.0)
+
+        # 세 번째는 차량을 멈추고 보행자가 건널 수 있게 바꾼 상태다.
+        # sleep 시간을 주면서 신호가 순서대로 반복되도록 만드는 것도 같이 연습했다.
         leds.value = (1,0,0,0,1)
         sleep(3.0)
     
 except KeyboardInterrupt:
+    # 실행하다가 Ctrl+C로 멈출 수도 있어서 예외 처리도 같이 넣어봤다.
     pass
 
+# 프로그램이 끝날 때는 LED가 계속 켜져 있지 않게 마지막에 전부 꺼주도록 했다.
 leds.off()
--- project_6/main6-2.py
+++ project_6/main6-2.py
@@ -1,21 +1,29 @@
 from gpigzero import DigitalINputDevice
 from gpiozero import OutputDevice
 import time
+
+# 이번 실습에서는 가스 센서 값을 읽고, 그 값에 따라 부저가 울리게 만드는 흐름을 연습했다.
+# 센서는 입력 장치이고 부저는 출력 장치라는 점도 같이 구분해봤다.
 bz = OutputDevice (18)
 gas=DigitalINputDevice (17)
 
 try:
     while True:
+        # 센서 값이 0일 때를 가스가 감지된 상황으로 보고 부저가 바로 울리게 했다.
         if gas.value == 0:
             bz.on()
             print("Gas Detected")
         else:
-            
+            # 반대로 감지되지 않으면 부저를 끄고 현재 상태를 출력하게 했다.
             print("No Gas Detected")
             bz.off()
 
+        # 너무 빠르게 반복하지 않게 짧게 쉬면서 센서 상태를 계속 확인하게 만들었다.
         time.sleep(0.2)
 
 except KeyboardInterrupt:
+    # 실행 중에 멈출 수도 있어서 종료할 때를 대비한 예외 처리도 넣었다.
     pass
+
+# 끝날 때 부저가 계속 울리면 안 되니까 마지막에 꼭 꺼주도록 했다.
 bz.off()
